c215a0dd7dea535d9a6f0f49a5433ef0bf060653
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-09 13:59:10 +0000

    Changed module token from "javafx" to "openjfx" when designating OpenJFX runtime

--- webfx-demo-tallycounter-application-gluon/src/main/java/module-info.java
+++ webfx-demo-tallycounter-application-gluon/src/main/java/module-info.java
@@ -4,8 +4,9 @@ module webfx.demo.tallycounter.application.gluon {
 
     // Direct dependencies modules
     requires webfx.demo.tallycounter.application;
-    requires webfx.kit.javafx;
+    requires webfx.kit.openjfx;
     requires webfx.platform.java.appcontainer.impl;
+    requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.shared.log.impl.simple;
 
--- webfx-demo-tallycounter-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-tallycounter-application-openjfx/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 // File managed by WebFX (DO NOT EDIT MANUALLY)
 
-module webfx.demo.tallycounter.application.javafx {
+module webfx.demo.tallycounter.application.openjfx {
 
     // Direct dependencies modules
     requires webfx.demo.tallycounter.application;
-    requires webfx.kit.javafx;
+    requires webfx.kit.openjfx;
     requires webfx.platform.java.appcontainer.impl;
+    requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.shared.log.impl.simple;
 
